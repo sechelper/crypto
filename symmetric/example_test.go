ae80ee2b6c2e3d0f0c8fd6dd0918ddf3a66cf673
cookun <cookun@sechelper.com>
2023-10-12 08:42:57 +0800

    update

--- symmetric/example_test.go
+++ symmetric/example_test.go
@@ -1,29 +1,29 @@
-package symmetric
+package symmetric_test
 
 import (
 	"encoding/base64"
 	"fmt"
-	"testing"
+	"github.com/sechelper/crypto/symmetric"
 )
 
-func TestDes3ECBEncrypt(t *testing.T) {
+func ExampleDes3ECBEncrypt() {
 	key := []byte("012345678901234567890123") // 24-byte key for 3DES
 
 	plaintext := []byte("Hello, Secself!") // Plaintext to encrypt
-	encrypt, err := Des3ECBEncrypt(key, plaintext)
+	encrypt, err := symmetric.Des3ECBEncrypt(key, plaintext)
 	if err != nil {
-		t.Error(err)
+		panic(err)
 	}
 
 	fmt.Println(base64.StdEncoding.EncodeToString(encrypt))
 
 }
-func TestDes3ECBDecrypt(t *testing.T) {
+func ExampleDes3ECBDecrypt() {
 	key := []byte("012345678901234567890123") // 24-byte key for 3DES
 	encrypt, _ := base64.StdEncoding.DecodeString("6Dxl7vNNWD4G2sGt6JN1RA==")
-	decrypt, err := Des3ECBDecrypt(key, encrypt)
+	decrypt, err := symmetric.Des3ECBDecrypt(key, encrypt)
 	if err != nil {
-		t.Error(err)
+		panic(err)
 	}
 	fmt.Println(string(decrypt))
 }
